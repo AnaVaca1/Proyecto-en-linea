c51b009fbaff8d56ec3faedf332093e0642f6037
Rodinson Parra <rodinzon@live.com>
2017-05-16 14:41:38 -0500

    Subidos archivos: .html, .css, .js

--- general.js
+++ general.js
@@ -1,1 +1,8 @@
-
+	function comprobar (){
+		var nombre = document.forms["formulario"]["nombre"].value;
+		var email = document.forms["formulario"]["email"].value;
+		if (nombre == "" || email == ""){
+			alert("Uno o mas campos estan vacios");
+			return false;
+		}
+	}
